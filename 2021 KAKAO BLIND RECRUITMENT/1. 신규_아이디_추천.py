5f9d8bb54f7b5de4fcc9832baf3d4c0f32714f6a
heewoo0419 <heewoo0419@gmail.com>
2021-01-29 20:34:30 +0900

    정규표현식

---
+++ 2021 KAKAO BLIND RECRUITMENT/1. 신규_아이디_추천.py
@@ -0,0 +1,44 @@
+import re
+
+
+def solution(new_id):
+    # 1단계 - 소문자 변경
+    new_id = new_id.lower()
+
+    # 2단계 - 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거
+    new_id = re.sub('[^a-z0-9\-\_\.]', "", new_id)
+
+    # 3단계 - 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
+    new_id = re.sub('[.]+', '.', new_id)
+
+    # 4단계 - 마침표(.)가 처음이나 끝에 위치한다면 제거
+    if len(new_id) > 0:
+        if new_id[0] == '.':
+            new_id = new_id[1:]
+
+    if len(new_id) > 0:
+        if new_id[-1] == '.':
+            new_id = new_id[:- 1]
+
+    # 5단계 - new_id가 빈 문자열이라면, new_id에 "a"를 대입
+    new_id = 'a' if len(new_id) == 0 else new_id
+
+    # 6단계 - new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
+    if len(new_id) > 15:
+        new_id = new_id[:15]
+        # 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거
+        if new_id[-1] == '.':
+            new_id = new_id[:-1]
+
+    # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙임
+    if len(new_id) < 3:
+        new_id += new_id[-1] * (3 - len(new_id))
+
+    return new_id
+
+
+print(solution("...!@BaT#*..y.abcdefghijklm"))
+print(solution("z-+.^."))
+print(solution("=.="))
+print(solution("123_.def"))
+print(solution("abcdefghijklmn.p"))
